3c229333ad8bfd3e9e68b5275c4d0e63be647a0a
Ooozari <uzairasiff1227@gmail.com>
2025-08-25 14:01:41 +0500

    style(not-found): update the style

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,18 @@
 'use client';
+import { Heading, Paragraph } from '@/components/ui/typography';
+import { SearchX } from 'lucide-react'
+import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4 gap-[16px] sm:gap-[20px] md:gap-[24px] bg-[#a02828] font-open">
-      not found
+    <div className="flex flex-col  justify-center items-center text-center bg-primary-page-bg w-full h-[100vh] gap-10">
+      <Heading level='h1' className='font-roboto text-blue-600 font-[900]'>
+        404 {':('}
+      </Heading>
+      <span ><SearchX className='text-extraLight w-20 h-20' /></span>
+      <Paragraph size='xl' className='text-light font-medium'>
+        Page not found <Link href='/' className='text-positive font-[600] underline'>back to home</Link>
+      </Paragraph>
     </div>
   );
 }
